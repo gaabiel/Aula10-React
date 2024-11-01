4a066e406d9ac950253e4fb21adc6c20df209887
Felipe Gabriel <felipe.gabi.souz@gmail.com>
2024-11-01 10:45:26 -0300

    0111

--- escola/src/Componentes/Rotas.jsx
+++ escola/src/Componentes/Rotas.jsx
@@ -9,6 +9,14 @@ import Erro from '../Paginas/Erro';
 
 export default function Rotas(){
     return ( 
-        
+        <BrowserRouter>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/consulta" element={<Consulta />} />        
+            <Route path="/alteracao/:codigo" element={<Alteracao />} />        
+            <Route path="/exclusao/:codigo" element={<Exclusao />} />        
+            <Route path="*" element={<Erro />} />        
+
+        </BrowserRouter>
     );
 }
